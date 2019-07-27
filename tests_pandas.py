165cdf6ec004d515cbf24a5cf45c8edf04101353
Shrikrishna Kadam <shrikrishnakadam@Shrikrishnas-MacBook-Pro.local>
2019-07-27 13:38:22 +0530

    Changed tests

--- tests_pandas.py
+++ tests_pandas.py
@@ -23,20 +23,19 @@ class PandaCSVTestCase(TestCase):
         self.assertEqual(json_resp.get('result'), 'top-n value should be an integer')
 
     def test_get_top_n_failure_exit_codes(self):
-        resp = self.client.post('/top_n_failure_code', json={'top': 1})
+        resp = self.client.post('/top_n_failure_code', json={'top': 2})
         json_resp = resp.get_json().get('result')
-        self.assertEqual(len(json_resp.get('exit_codes')), 1)
+        self.assertEqual(len(json_resp.get('exit_codes')), 2)
         self.assertEqual(json_resp.get('exit_codes')[0], 1)
 
     def test_get_build_success_rate(self):
         resp = self.client.get('/build_success_rate')
         json_resp = resp.get_json()
-        self.assertEqual(json_resp.get('percentage_success_rate'), 75.0)
+        self.assertEqual(json_resp.get('percentage_success_rate'), 62.5)
 
     def test_build_count_in_time_window(self):
         resp = self.client.post('/builds_in_window', json={'start_date': '1/11/2018',
                                                            'end_date': '3/11/2018'})
-        import pdb;pdb.set_trace()
         result = resp.get_json().get('result')
         self.assertEqual(result['count'], 4)
         self.assertTrue('5c00a8f685db9ec46dbc13dc' in result['uid_list'])
